a253e279b207e3ef59e386808d3b9ad6b8ffd51d
Syed Umair Ali <57952190+SyedUmairCodes@users.noreply.github.com>
2022-05-09 17:36:55 +0500

    Mobile header completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,24 @@
 // Variable declarations
-const openMenuIcon = document.getElementById('menu-toggle-open');
-const closeMenuIcon = document.getElementById('menu-toggle-close');
-const mobileMenu = document.getElementById('mobile-menu');
-const headerOverlay = document.getElementById('header');
-const grassImage = document.getElementById('grassland');
-const skyImage = document.getElementById('sky');
-const mountainsImage = document.getElementById('mountains');
-const heroData = document.getElementById('hero-data');
+const openMenuIcon = document.getElementById("menu-toggle-open");
+const closeMenuIcon = document.getElementById("menu-toggle-close");
+const mobileMenu = document.getElementById("mobile-menu");
+const headerOverlay = document.getElementById("header");
+const grassImage = document.getElementById("grassland");
+const skyImage = document.getElementById("sky");
+const mountainsImage = document.getElementById("mountains");
+const heroData = document.getElementById("hero-data");
 
 // Open menu & add overlay
+
+openMenuIcon.addEventListener("click", () => {
+  mobileMenu.classList.add("menu-opened");
+  headerOverlay.classList.add("menu-opened");
+});
+
+closeMenuIcon.addEventListener("click", () => {
+  mobileMenu.classList.remove("menu-opened");
+  headerOverlay.classList.remove("menu-opened");
+});
 
 // Close menu & remove overlay
 
